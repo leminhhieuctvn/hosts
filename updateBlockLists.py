98f91c47dc8ca8d0a36f96a28a3c1e620857d4c1
leminhhieuctvn <leminhhieuct@gmail.com>
2024-06-07 13:22:53 +0700

    Update

--- updateBlockLists.py
+++ updateBlockLists.py
@@ -10,14 +10,11 @@ def download_list(url):
 def parse_list(raw_list):
     lines = raw_list.splitlines()
     filtered_lines = set()
-    
-    # Regex to match lines starting with "!" followed by one or more spaces
-    comment_pattern = re.compile(r'^!\s+')
 
     # Filter out comments and empty lines
     for line in lines:
         stripped_line = line.strip()
-        if stripped_line and not comment_pattern.match(stripped_line):
+        if stripped_line:
             filtered_lines.add(stripped_line)
     
     return filtered_lines
@@ -38,19 +35,14 @@ def run_fanboy_sorter(perl_script, input_file):
 # URLs to download blocklists and whitelists
 blocklist_urls = [
     ('https://raw.githubusercontent.com/abpvn/abpvn/master/filter/abpvn_ublock.txt'),
-    ('https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/filters.min.txt'),
-    ('https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/badware.min.txt'),
-    ('https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/privacy.min.txt'),
-    ('https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/quick-fixes.min.txt'),
-    ('https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/unbreak.min.txt'),
-    ('https://raw.githubusercontent.com/uBlockOrigin/uAssets/gh-pages/filters/annoyances-cookies.txt'),
-    ('https://easylist.to/easylist/easylist.txt'),
-    ('https://easylist.to/easylist/easyprivacy.txt'),
-    ('https://secure.fanboy.co.nz/fanboy-cookiemonster.txt'),
-    ('https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/platforms/extension/ublock/filters/2_without_easylist.txt'),
-    ('https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/platforms/extension/ublock/filters/3_optimized.txt'),
-    ('https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/platforms/extension/ublock/filters/17_optimized.txt'),
-    ('https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/platforms/extension/ublock/filters/18_optimized.txt')
+    ('https://cdn.jsdelivr.net/gh/uBlockOrigin/uAssetsCDN@main/filters/filters.min.txt'),
+    ('https://ublockorigin.github.io/uAssetsCDN/filters/badware.min.txt'),
+    ('https://ublockorigin.github.io/uAssetsCDN/filters/privacy.min.txt'),
+    ('https://cdn.jsdelivr.net/gh/uBlockOrigin/uAssetsCDN@main/filters/quick-fixes.min.txt'),
+    ('https://cdn.statically.io/gh/uBlockOrigin/uAssetsCDN/main/filters/unbreak.min.txt'),
+    ('https://cdn.statically.io/gh/uBlockOrigin/uAssetsCDN/main/thirdparties/easylist.txt'),
+    ('https://filters.adtidy.org/extension/ublock/filters/2_without_easylist.txt'),
+    ('https://cdn.statically.io/gh/uBlockOrigin/uAssetsCDN/main/thirdparties/easyprivacy.txt')
 ]
 
 # whitelist_urls = [
